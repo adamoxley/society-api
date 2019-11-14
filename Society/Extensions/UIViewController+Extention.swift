4169046fdfe56f077898cee14ae3179f914b5ec9
Adam Oxley <adam@adamoxley.uk>
2019-11-14 23:26:55 +0000

    Updating navigation bar style handling

--- Society/Extensions/UIViewController+Extention.swift
+++ Society/Extensions/UIViewController+Extention.swift
@@ -16,6 +16,19 @@ extension UIViewController {
         view.addGestureRecognizer(tap)
     }
     
+    func whiteNavigation(displayDividerLine: Bool = false) {
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        if displayDividerLine {
+            navigationController?.navigationBar.shadowImage = UIImage(named: "grey_divider_line")
+        }
+    }
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
